2da9115cb9b9d1ea61dbdbf6adcadc58d7340625
Ziineb <40890004+Ziineb@users.noreply.github.com>
2018-07-10 16:10:27 +0000

    Add files via upload

---
+++ test4.js
@@ -0,0 +1,15 @@
+
+function myFun(){
+	document.getElementById('title').innerHTML="Goodbye";
+	var couleur = document.getElementById('title').style.color;
+	if (couleur=="blue") {
+			document.getElementById('title').style.color="red";
+	}
+	else{
+		document.getElementById('title').style.color="blue";
+	}
+
+	}
+
+
+	
